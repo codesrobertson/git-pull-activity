284eac987797123738c36280f441210a5ec6560c
Alex Robertson <arobertson335@gmail.com>
2020-02-12 12:20:08 -0800

    Completed first step.

--- pull_practice.rb
+++ pull_practice.rb
@@ -4,12 +4,12 @@
 # code rather than writing it, but the same principals will apply.
 
 # Task 1: Pulling in changes
-# One partner should uncomment this method, add and
-# commit the changes and push to GitHub. The other
-# partner should run git pull to retrieve the changes.
-# def duck_noise
-#   puts "quack"
-# end
+One partner should uncomment this method, add and
+commit the changes and push to GitHub. The other
+partner should run git pull to retrieve the changes.
+def duck_noise
+  puts "quack"
+end
 
 # Task 2: Same as task one, but switch roles
 # def truck_noise
